34769c509910bf3ab09898a31854a6317fa22178
Peng Gu <penggu@gmail.com>
2018-02-10 00:46:41 -0800

    Update ssh-no-password.sh
    minor change in comments.

--- aliyun/ecs/ubuntu/ssh-no-password.sh
+++ aliyun/ecs/ubuntu/ssh-no-password.sh
@@ -1,4 +1,5 @@
-# Refer to https://help.ubuntu.com/community/SSH/OpenSSH/Keys
 export user=$1
 export host=$2
 ssh-copy-id $user@$host
+
+# Credit: https://help.ubuntu.com/community/SSH/OpenSSH/Keys
